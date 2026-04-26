28d297f23b4b940c0908c87ee460a9190adbc381
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-04-26 10:02:35 +0600

    cache implements

---
+++ Module 4- Algorithoms in action/basic cache implementation with map.js
@@ -0,0 +1,25 @@
+const dataCache = new Map();
+
+const expensiveTask = (id) => {
+  console.log("Run the expensive task for: ", id);
+  return {
+    id: id,
+    data: `Some data for id ${id}`,
+    timestamp: new Date().getTime(),
+  };
+};
+const getData = (id) => {
+  if (dataCache.has(id)) {
+    console.log("cache hit for id: ", id);
+    return dataCache.get(id);
+  }
+  //   return expensiveTask(id);
+  console.log("cache miss for id: ", id);
+  const data = expensiveTask(id);
+  dataCache.set(id, data);
+  return data;
+};
+console.log(dataCache);
+console.log(getData(1234));
+console.log(getData(1234));
+console.log(dataCache);
